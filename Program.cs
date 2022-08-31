bb8b551c5d3312240f506ccf8e4d07efc801ecf9
Giancarlo Culcay <gieg5c@gmail.com>
2022-08-30 22:53:20 -0500

    Conectado a base de datos SQL server

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@ using proyectoef;   //el namespace de TareasContext
 
 var builder = WebApplication.CreateBuilder(args);
 
-//
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+//db en memoria:
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+//de en Sql Server:
+// con pass:
+//builder.Services.AddSqlServer<TareasContext>("Data Source=LAPTOP-2FO7TIND\\SQLEXPRESS;Initial Catalog=TareasDb;user id=sa;password=dominic");
+// sin pass, con windows authentication:
+builder.Services.AddSqlServer<TareasContext>("Data Source=(local); Initial Catalog= TareasDb;Trusted_Connection=True; Integrated Security=True");
 
 var app = builder.Build(); //la aplicacion se construye
 
@@ -18,6 +24,7 @@ app.MapGet("/dbconexion", async([FromServices] TareasContext dbContext) =>
     dbContext.Database.EnsureCreated();  
     return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
     // IsInMemory: da True/False si la db se creo con el EnsureCreated
+    //return Results.Ok("Base de datos en SQL Server: " + dbContext.Database.IsSqlServer());
 });
 
 app.Run();
